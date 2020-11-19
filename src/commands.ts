73674646a27e0ceaf75cdb61ffff07aeb15a776c
hiroki <hiroki.osame@gmail.com>
2020-11-19 03:31:39 -0500

    feat(ni): flag --freeze-if-present (#6)
    * feat: added flag --frozen=safe
    
    * feat: use lock file detection for freeze-if-present

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,7 @@
 import { Agent, AGENTS, Command } from './agents'
 import { exclude } from './utils'
 
-export function getCommand(agent: Agent, commnad: Command, args: string[]) {
+export function getCommand(agent: Agent, commnad: Command, args: string[] = []) {
   if (!(agent in AGENTS))
     throw new Error(`Unsupported agent "${agent}"`)
 
@@ -13,28 +13,22 @@ export function getCommand(agent: Agent, commnad: Command, args: string[]) {
   return c.replace('{0}', args.join(' '))
 }
 
-export function parseNi(agent: Agent, _args: string[]): string {
-  let command: Command = 'install'
-  let args: string[] = []
+export function parseNi(agent: Agent, args: string[], hasLock?: boolean): string {
+  if (args.length === 0)
+    return getCommand(agent, 'install')
 
-  if (_args.length === 0) {
-    command = 'install'
-    args = []
-  }
-  else if (_args.includes('-g')) {
-    command = 'global'
-    args = exclude(_args, '-g')
-  }
-  else if (_args.includes('--frozen')) {
-    command = 'frozen'
-    args = exclude(_args, '--frozen')
-  }
-  else {
-    command = 'add'
-    args = _args
+  if (args.includes('-g'))
+    return getCommand(agent, 'global', exclude(args, '-g'))
+
+  if (args.includes('--frozen'))
+    return getCommand(agent, 'frozen', exclude(args, '--frozen'))
+
+  if (args.includes('--frozen-if-present')) {
+    args = exclude(args, '--frozen-if-present')
+    return getCommand(agent, hasLock ? 'frozen' : 'install', args)
   }
 
-  return getCommand(agent, command, args)
+  return getCommand(agent, 'add', args)
 }
 
 export function parseNr(agent: Agent, args: string[]): string {
--- src/runner.ts
+++ src/runner.ts
@@ -7,19 +7,23 @@ import { remove } from './utils'
 const args = process.argv.slice(2).filter(Boolean)
 const DEBUG_SIGN = '?'
 
-export type Runner = (agent: Agent, args: string[], debug: boolean) => Promise<string>
+export type Runner = (agent: Agent, args: string[], hasLock?: boolean) => Promise<string>
 
 export async function run(fn: Runner) {
-  const isGlobal = args.includes('-g')
-  const agent = isGlobal
-    ? await getDefaultAgent()
-    : await detect()
-
   const debug = args.includes(DEBUG_SIGN)
   if (debug)
     remove(args, DEBUG_SIGN)
 
-  const command = await fn(agent, args, debug)
+  const isGlobal = args.includes('-g')
+  let command
+
+  if (isGlobal) {
+    command = await fn(await getDefaultAgent(), args)
+  }
+  else {
+    const agent = await detect()
+    command = await fn(agent || await getDefaultAgent(), args, Boolean(agent))
+  }
 
   if (debug)
     console.log(command)
