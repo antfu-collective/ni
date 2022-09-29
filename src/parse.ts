96e29dc587d8671acffc5028881f0ca81f6fccfa
Anthony Fu <anthonyfu117@hotmail.com>
2022-09-30 00:40:15 +0800

    chore: update

--- src/parse.ts
+++ src/parse.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+import { bold, dim, green, yellow } from 'kleur'
 import { version } from '../package.json'
 import type { Agent, Command } from './agents'
 import { AGENTS } from './agents'
@@ -23,30 +25,22 @@ export function getCommand(
 }
 
 export const parseNi = <Runner>((agent, args, ctx) => {
-  if (args.length === 1 && args[0] === '-v') {
-    // eslint-disable-next-line no-console
+  if (args.length === 1 && (args[0] === '--version' || args[0] === '-v')) {
     console.log(`@antfu/ni v${version}`)
     process.exit(0)
   }
-  if (args.length === 1 && args[0] === '-help') {
-    // eslint-disable-next-line no-console
-    console.log('Intro: use the right package manager')
-    // eslint-disable-next-line no-console
-    console.log('ni - install')
-    // eslint-disable-next-line no-console
-    console.log('nr - run')
-    // eslint-disable-next-line no-console
-    console.log('nx - execute')
-    // eslint-disable-next-line no-console
-    console.log('nu - upgrade')
-    // eslint-disable-next-line no-console
-    console.log('nun - uninstall')
-    // eslint-disable-next-line no-console
-    console.log('nci - clean install')
-    // eslint-disable-next-line no-console
-    console.log('na - agent alias')
-    // eslint-disable-next-line no-console
-    console.log('Visit https://github.com/antfu/ni for documentation about this command.\n')
+
+  if (args.length === 1 && (args[0] === '--help' || args[0] === '-h')) {
+    const dash = dim('-')
+    console.log(green(bold('ni')) + dim(' use the right package manager\n'))
+    console.log(`ni   ${dash}  install`)
+    console.log(`nr   ${dash}  run`)
+    console.log(`nx   ${dash}  execute`)
+    console.log(`nu   ${dash}  upgrade`)
+    console.log(`nun  ${dash}  uninstall`)
+    console.log(`nci  ${dash}  clean install`)
+    console.log(`na   ${dash}  agent alias`)
+    console.log(yellow('\ncheck https://github.com/antfu/ni for more documentation.'))
     process.exit(0)
   }
 
