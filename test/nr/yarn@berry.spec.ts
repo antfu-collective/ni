cf30a6f57fe8485bc3c9bdd46d839f702335af41
Xing Yahao <keidarcy.1015@gmail.com>
2022-10-05 22:26:22 +0900

    test: add missing yarn berry test

---
+++ test/nr/yarn@berry.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest'
+import { parseNr } from '../../src/commands'
+
+const agent = 'yarn@berry'
+const _ = (arg: string, expected: string) => () => {
+  expect(
+    parseNr(agent, arg.split(' ').filter(Boolean)),
+  ).toBe(
+    expected,
+  )
+}
+
+test('empty', _('', 'yarn run start'))
+
+test('if-present', _('test --if-present', 'yarn run --if-present test'))
+
+test('script', _('dev', 'yarn run dev'))
+
+test('script with arguments', _('build --watch -o', 'yarn run build --watch -o'))
+
+test('colon', _('build:dev', 'yarn run build:dev'))
