4e97fdf5be4a546c106ae901ea4b8711c1f7d86b
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-23 12:00:17 +0200

    chore: update deps

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,13 @@
+import { basename } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
 import fg from 'fast-glob'
 
 export default defineBuildConfig({
   entries: [
-    ...fg.sync('src/commands/*.ts').map(i => i.slice(0, -3)),
+    ...fg.sync('src/commands/*.ts').map(i => ({
+      input: i.slice(0, -3),
+      name: basename(i).slice(0, -3),
+    })),
   ],
   clean: true,
   declaration: true,
