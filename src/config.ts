760598e5873efeea809f54ab2db419aa76689a64
Ray <i@mk1.io>
2023-11-27 18:53:08 +0800

    fix: correctly override local agent (#188)

--- src/config.ts
+++ src/config.ts
@@ -29,19 +29,16 @@ let config: Config | undefined
 
 export async function getConfig(): Promise<Config> {
   if (!config) {
+    config = Object.assign(
+      {},
+      defaultConfig,
+      fs.existsSync(rcPath)
+        ? ini.parse(fs.readFileSync(rcPath, 'utf-8'))
+        : null,
+    )
     const agent = await detect({ programmatic: true })
-    if (agent) {
-      config = { ...defaultConfig, defaultAgent: agent }
-    }
-    else {
-      config = Object.assign(
-        {},
-        defaultConfig,
-        fs.existsSync(rcPath)
-          ? ini.parse(fs.readFileSync(rcPath, 'utf-8'))
-          : null,
-      )
-    }
+    if (agent)
+      config.defaultAgent = agent
   }
 
   return config
