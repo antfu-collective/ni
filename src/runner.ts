50362364bde9fefaf05517a049effc7375b20596
SukkaW <isukkaw@gmail.com>
2024-06-26 16:03:57 +0800

    refactor: replace `execa` with `ezspawn`

--- src/runner.ts
+++ src/runner.ts
@@ -2,8 +2,8 @@
 import { resolve } from 'node:path'
 import process from 'node:process'
 import prompts from '@posva/prompts'
-import type { Options as ExecaOptions } from 'execa'
-import { execaCommand } from 'execa'
+import type { Options as EzSpawnOptions } from '@jsdevtools/ez-spawn'
+import { async as ezspawn } from '@jsdevtools/ez-spawn'
 import c from 'kleur'
 import { version } from '../package.json'
 import type { Agent } from './agents'
@@ -86,7 +86,7 @@ export async function run(fn: Runner, args: string[], options: DetectOptions = {
 
   if (args.length === 1 && (args[0]?.toLowerCase() === '-v' || args[0] === '--version')) {
     const getCmd = (a: Agent) => agents.includes(a) ? getCommand(a, 'agent', ['-v']) : `${a} -v`
-    const getV = (a: string, o?: ExecaOptions) => execaCommand(getCmd(a as Agent), o).then(e => e.stdout).then(e => e.startsWith('v') ? e : `v${e}`)
+    const getV = (a: string, o: EzSpawnOptions = {}) => ezspawn(getCmd(a as Agent), o).then(e => e.stdout).then(e => e.startsWith('v') ? e : `v${e}`)
     const globalAgentPromise = getGlobalAgent()
     const globalAgentVersionPromise = globalAgentPromise.then(getV)
     const agentPromise = detect({ ...options, cwd }).then(a => a || '')
@@ -139,5 +139,5 @@ export async function run(fn: Runner, args: string[], options: DetectOptions = {
     return
   }
 
-  await execaCommand(command, { stdio: 'inherit', cwd })
+  await ezspawn(command, { stdio: 'inherit', cwd })
 }
