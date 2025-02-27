8343421673301fd202fb7b43db00462e61b2e50a
Ezira Ashenafi <eazash22@gmail.com>
2025-02-27 23:07:18 +0300

    feat: add zsh completion support

--- src/completion.ts
+++ src/completion.ts
@@ -1,5 +1,5 @@
-// Print completion script
-export const rawCompletionScript = `
+// Raw completion scripts for bash and zsh
+export const rawBashCompletionScript = `
 ###-begin-nr-completion-###
 
 if type complete &>/dev/null; then
@@ -15,3 +15,30 @@ fi
 
 ###-end-nr-completion-###
 `.trim()
+
+export const rawZshCompletionScript = `
+_nr(){
+  # Set COMP_CWORD to one less than the actual current index (since the first word is the command itself)
+  out=($(COMP_CWORD=$((\${CURRENT} - 1)) COMP_LINE="\${words[@]}" nr --completion "\${words[@]}"))
+  compadd -a out
+}
+
+# don't run the completion function when being source-ed or eval-ed
+if [ "$funcstack[1]" = "_nr" ]; then
+  _nr
+fi
+`.trim()
+
+export function printCompletionScript(shell: string = 'bash') {
+  if (shell === 'bash') {
+    // eslint-disable-next-line no-console
+    console.log(rawBashCompletionScript)
+  }
+  else if (shell === 'zsh') {
+    // eslint-disable-next-line no-console
+    console.log(rawZshCompletionScript)
+  }
+  else {
+    throw new Error(`Unsupported shell '${shell}'`)
+  }
+}
