1ffc2aaefd571426b78576522a5ddc5b7d639d61
KaBankz <20971850+KaBankz@users.noreply.github.com>
2025-05-26 22:12:26 -0400

    test: update tests

--- test/nup/npm.spec.ts
+++ test/nup/npm.spec.ts
@@ -1,11 +1,11 @@
 import { expect, it } from 'vitest'
-import { parseNu, serializeCommand } from '../../src/commands'
+import { parseNup, serializeCommand } from '../../src/commands'
 
 const agent = 'npm'
 function _(arg: string, expected: string) {
   return async () => {
     expect(
-      serializeCommand(await parseNu(agent, arg.split(' ').filter(Boolean))),
+      serializeCommand(await parseNup(agent, arg.split(' ').filter(Boolean))),
     ).toBe(
       expected,
     )
