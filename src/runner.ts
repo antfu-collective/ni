1d0f68df873b90871a4d7b67cf420a31e38163d3
liangmiQwQ <mi_yyds@126.com>
2025-09-24 19:37:35 +0800

    revert: refactor: use detached instead of treekill

--- src/runner.ts
+++ src/runner.ts
@@ -13,7 +13,7 @@ import { getDefaultAgent, getGlobalAgent } from './config'
 import { detect } from './detect'
 import { getEnvironmentOptions } from './environment'
 import { getCommand, UnsupportedCommand } from './parse'
-import { cmdExists, remove } from './utils'
+import { cmdExists, remove, treeKill } from './utils'
 
 const DEBUG_SIGN = '?'
 
@@ -163,16 +163,28 @@ export async function run(fn: Runner, args: string[], options: DetectOptions = {
     return
   }
 
-  await x(
+  const proc = x(
     command.command,
     command.args,
     {
       nodeOptions: {
         stdio: 'inherit',
         cwd: command.cwd ?? cwd,
-        detached: true,
       },
       throwOnError: true,
     },
   )
+
+  process.once('SIGINT', () => {
+    if (proc.pid) {
+      treeKill(proc.pid, 'SIGINT')
+    }
+    else {
+      proc.kill('SIGINT')
+    }
+
+    process.exit(130)
+  })
+
+  await proc
 }
